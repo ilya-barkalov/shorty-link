77b9d964851e4ed98f8104236f06996bd7eceb96
ilya-barkalov <life.barkalov@gmail.com>
2023-09-05 00:24:32 +0300

    add few tests

--- SL.WebAPI.UnitTests/Controllers/LinkControllerTests.cs
+++ SL.WebAPI.UnitTests/Controllers/LinkControllerTests.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using SL.Application.Common.Exceptions;
+using SL.Application.Features.Commands.CreateLink;
+using SL.Application.Features.Commands.DeleteLink;
+using SL.Application.Features.Commands.UpdateLink;
 using SL.Application.Features.Queries.GetLink;
 using SL.WebAPI.Controllers;
 
@@ -96,6 +99,244 @@ public class LinkControllerTests
         
         mediatorMock.Verify(
             x => x.Send(It.IsAny<GetLinkQuery>(), It.IsAny<CancellationToken>()), 
+            Times.Once);
+    }
+
+    [Fact]
+    public async Task CreateLink_OnSuccess_ReturnsOkWithGuid()
+    {
+        // Arrange
+        var linkId = Guid.NewGuid();
+        
+        var mediatorMock = new Mock<IMediator>();
+        mediatorMock.Setup(i => i.Send(It.IsAny<CreateLinkCommand>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(() => linkId);
+        
+        var sut = new LinkController(mediatorMock.Object);
+
+        // Act
+        var action = await sut.CreateLink(new CreateLinkCommand("url"), CancellationToken.None);
+
+        // Assert
+        action.Should().BeOfType<OkObjectResult>();
+        
+        var result = action.As<OkObjectResult>();
+        result.StatusCode.Should().Be(StatusCodes.Status200OK);
+        result.Value.Should().BeOfType<Guid>();
+                
+        mediatorMock.Verify(
+            x => x.Send(It.IsAny<CreateLinkCommand>(), It.IsAny<CancellationToken>()), 
+            Times.Once);
+    }
+    
+    [Fact]
+    public async Task CreateLink_OnValidationException_ReturnsBadRequest()
+    {
+        // Arrange
+        var guid = Guid.NewGuid();
+        var validationExceptionMessage = $"Link with Id {guid} was not found";
+        
+        var mediatorMock = new Mock<IMediator>();
+        mediatorMock.Setup(i => i.Send(It.IsAny<CreateLinkCommand>(), It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new ValidationException(validationExceptionMessage));
+        
+        var sut = new LinkController(mediatorMock.Object);
+
+        // Act
+        var action = await sut.CreateLink(new CreateLinkCommand("url"), CancellationToken.None);
+
+        // Assert
+        action.Should().BeOfType<BadRequestObjectResult>();
+        
+        var result = action.As<BadRequestObjectResult>();
+        result.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
+        result.Value.As<string>().Should().Be(validationExceptionMessage);
+        
+        mediatorMock.Verify(
+            x => x.Send(It.IsAny<CreateLinkCommand>(), It.IsAny<CancellationToken>()), 
+            Times.Once);
+    }
+    
+    [Fact]
+    public async Task CreateLink_OnException_ReturnsStatusCode500()
+    {
+        // Arrange
+        const string exceptionMessage = $"Some exception message";
+        
+        var mediatorMock = new Mock<IMediator>();
+        mediatorMock.Setup(i => i.Send(It.IsAny<CreateLinkCommand>(), It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new Exception(exceptionMessage));
+        
+        var sut = new LinkController(mediatorMock.Object);
+
+        // Act
+        var action = await sut.CreateLink(new CreateLinkCommand("url"), CancellationToken.None);
+
+        // Assert
+        action.Should().BeOfType<ObjectResult>();
+        
+        var result = action.As<ObjectResult>();
+        result.StatusCode.Should().Be(StatusCodes.Status500InternalServerError);
+        result.Value.As<string>().Should().Be(exceptionMessage);
+        
+        mediatorMock.Verify(
+            x => x.Send(It.IsAny<CreateLinkCommand>(), It.IsAny<CancellationToken>()), 
+            Times.Once);
+    }
+    
+    [Fact]
+    public async Task UpdateLink_OnSuccess_ReturnsOk()
+    {
+        // Arrange
+        var mediatorMock = new Mock<IMediator>();
+        mediatorMock.Setup(i => i.Send(It.IsAny<UpdateLinkCommand>(), It.IsAny<CancellationToken>()));
+        
+        var sut = new LinkController(mediatorMock.Object);
+
+        // Act
+        var action = await sut.UpdateLink(new UpdateLinkCommand(Guid.NewGuid(), "new url"), CancellationToken.None);
+
+        // Assert
+        action.Should().BeOfType<OkResult>();
+        
+        var result = action.As<OkResult>();
+        result.StatusCode.Should().Be(StatusCodes.Status200OK);
+                
+        mediatorMock.Verify(
+            x => x.Send(It.IsAny<UpdateLinkCommand>(), It.IsAny<CancellationToken>()), 
+            Times.Once);
+    }
+    
+    [Fact]
+    public async Task UpdateLink_OnNotFoundFoundException_ReturnsNotFound()
+    {
+        // Arrange
+        var guid = Guid.NewGuid();
+        var notFoundMessage = $"Link with Id {guid} was not found";
+        
+        var mediatorMock = new Mock<IMediator>();
+        mediatorMock.Setup(i => i.Send(It.IsAny<UpdateLinkCommand>(), It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new NotFoundException(notFoundMessage));
+        
+        var sut = new LinkController(mediatorMock.Object);
+
+        // Act
+        var action = await sut.UpdateLink(new UpdateLinkCommand(guid, "new url"), CancellationToken.None);
+
+        // Assert
+        action.Should().BeOfType<NotFoundObjectResult>();
+        
+        var result = action.As<NotFoundObjectResult>();
+        result.StatusCode.Should().Be(StatusCodes.Status404NotFound);
+        result.Value.As<string>().Should().Be(notFoundMessage);
+        
+        mediatorMock.Verify(
+            x => x.Send(It.IsAny<UpdateLinkCommand>(), It.IsAny<CancellationToken>()), 
+            Times.Once);
+    }
+    
+    [Fact]
+    public async Task UpdateLink_OnException_ReturnsStatusCode500()
+    {
+        // Arrange
+        const string exceptionMessage = $"Some exception message";
+        
+        var mediatorMock = new Mock<IMediator>();
+        mediatorMock.Setup(i => i.Send(It.IsAny<UpdateLinkCommand>(), It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new Exception(exceptionMessage));
+        
+        var sut = new LinkController(mediatorMock.Object);
+
+        // Act
+        var action = await sut.UpdateLink(new UpdateLinkCommand(Guid.NewGuid(), "new url"), CancellationToken.None);
+
+        // Assert
+        action.Should().BeOfType<ObjectResult>();
+        
+        var result = action.As<ObjectResult>();
+        result.StatusCode.Should().Be(StatusCodes.Status500InternalServerError);
+        result.Value.As<string>().Should().Be(exceptionMessage);
+        
+        mediatorMock.Verify(
+            x => x.Send(It.IsAny<UpdateLinkCommand>(), It.IsAny<CancellationToken>()), 
+            Times.Once);
+    }
+    
+    [Fact]
+    public async Task DeleteLink_OnSuccess_ReturnsOk()
+    {
+        // Arrange
+        var mediatorMock = new Mock<IMediator>();
+        mediatorMock.Setup(i => i.Send(It.IsAny<DeleteLinkCommand>(), It.IsAny<CancellationToken>()));
+        
+        var sut = new LinkController(mediatorMock.Object);
+
+        // Act
+        var action = await sut.DeleteLink(new DeleteLinkCommand(Guid.NewGuid()), CancellationToken.None);
+
+        // Assert
+        action.Should().BeOfType<OkResult>();
+        
+        var result = action.As<OkResult>();
+        result.StatusCode.Should().Be(StatusCodes.Status200OK);
+                
+        mediatorMock.Verify(
+            x => x.Send(It.IsAny<DeleteLinkCommand>(), It.IsAny<CancellationToken>()), 
+            Times.Once);
+    }
+    
+    [Fact]
+    public async Task DeleteLink_OnNotFoundFoundException_ReturnsNotFound()
+    {
+        // Arrange
+        var guid = Guid.NewGuid();
+        var notFoundMessage = $"Link with Id {guid} was not found";
+        
+        var mediatorMock = new Mock<IMediator>();
+        mediatorMock.Setup(i => i.Send(It.IsAny<DeleteLinkCommand>(), It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new NotFoundException(notFoundMessage));
+        
+        var sut = new LinkController(mediatorMock.Object);
+
+        // Act
+        var action = await sut.DeleteLink(new DeleteLinkCommand(guid), CancellationToken.None);
+
+        // Assert
+        action.Should().BeOfType<NotFoundObjectResult>();
+        
+        var result = action.As<NotFoundObjectResult>();
+        result.StatusCode.Should().Be(StatusCodes.Status404NotFound);
+        result.Value.As<string>().Should().Be(notFoundMessage);
+        
+        mediatorMock.Verify(
+            x => x.Send(It.IsAny<DeleteLinkCommand>(), It.IsAny<CancellationToken>()), 
+            Times.Once);
+    }
+    
+    [Fact]
+    public async Task DeleteLink_OnException_ReturnsStatusCode500()
+    {
+        // Arrange
+        const string exceptionMessage = $"Some exception message";
+        
+        var mediatorMock = new Mock<IMediator>();
+        mediatorMock.Setup(i => i.Send(It.IsAny<DeleteLinkCommand>(), It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new Exception(exceptionMessage));
+        
+        var sut = new LinkController(mediatorMock.Object);
+
+        // Act
+        var action = await sut.DeleteLink(new DeleteLinkCommand(Guid.NewGuid()), CancellationToken.None);
+
+        // Assert
+        action.Should().BeOfType<ObjectResult>();
+        
+        var result = action.As<ObjectResult>();
+        result.StatusCode.Should().Be(StatusCodes.Status500InternalServerError);
+        result.Value.As<string>().Should().Be(exceptionMessage);
+        
+        mediatorMock.Verify(
+            x => x.Send(It.IsAny<DeleteLinkCommand>(), It.IsAny<CancellationToken>()), 
             Times.Once);
     }
 }
